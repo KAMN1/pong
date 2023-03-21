71b1091674071a1efa99aae0cef5b0820b565a38
KAMN1 <grechihin.yan@gmail.com>
2023-03-22 01:40:37 +0300

    2 player added

--- pong.py
+++ pong.py
@@ -2,8 +2,8 @@ import pygame
 
 # Initialization of the Game
 pygame.init()
-width = 1500
-height = 900
+width = 1100
+height = 600
 screen = pygame.display.set_mode((width, height)) # Width by Height
 
 # Player Stuff
@@ -19,29 +19,21 @@ ball_Y = height/2 - 15
 ball_dir_X = -3
 ball_dir_Y = 3
 
-# CPU Stuff
-cpu_img = pygame.image.load('stick.png')
-cpu_X = width - 10 - 27
-cpu_Y = height/2 - 100
+# Player2 Stuff
+player2_img = pygame.image.load('stick.png')
+player2_X = width-10-27
+player2_Y_default = height/2 - 100
+player2_Y_dif = 0
 
 def player(y_val):
     screen.blit(player_img, (player_X, y_val))
+    
+def player2(y_val):
+    screen.blit(player2_img, (player2_X, y_val))
 
 def ball():
     screen.blit(ball_img, (ball_X, ball_Y))
 
-def cpu():
-    screen.blit(cpu_img, (cpu_X, cpu_Y))
-
-def calc_cpu():
-    global cpu_Y
-    global ball_Y
-    if cpu_Y+100-ball_Y-15 > 0:
-        cpu_Y -= 3
-    elif cpu_Y+100-ball_Y-15 < 0:
-        cpu_Y += 3
-    cpu_Y = min(cpu_Y, height-10-200)
-    cpu_Y = max(cpu_Y, 10)
 
 def game_over(win):
     global ball_dir_X
@@ -51,8 +43,8 @@ def game_over(win):
     print(win)
     ball_X = width/2 - 15
     ball_Y = height/2 - 15
-    ball_dir_X = -3
-    ball_dir_Y = 3
+    ball_dir_X = -1
+    ball_dir_Y = 1
 
 def calc_ball():
     global ball_dir_X
@@ -65,14 +57,15 @@ def calc_ball():
         ball_dir_Y *= -1
     if ball_X <= 37 and ball_Y >= player_Y_default+player_Y_dif-30 and ball_Y <= player_Y_default+player_Y_dif+200 and ball_dir_X < 0:
         ball_dir_X *= -1
-    if ball_X >= cpu_X and ball_Y >= cpu_Y-11 and ball_Y <= cpu_Y+200-19 and ball_dir_X > 0:
+    if ball_X >= 1000 and ball_Y >= player2_Y_default+player2_Y_dif-11 and ball_Y <= player2_Y_default+player2_Y_dif+200-19 and ball_dir_X > 0:
         ball_dir_X *= -1
+    
     
     ball_X+=ball_dir_X
     ball_Y+=ball_dir_Y
     if (ball_X < 30):
         game_over(False)
-    elif (ball_X > cpu_X+10):
+    elif (ball_X > player2_X+30):
         game_over(True)
     ball_Y = min(ball_Y, height-10-30)
     ball_Y = max(ball_Y, 10)
@@ -86,6 +79,17 @@ def move_down(y_val):
     global player_Y_dif
     if player_Y_dif+y_val <= player_Y_default-10:
         player_Y_dif += y_val
+
+def move_up2(y_val):
+    global player2_Y_dif
+    if player2_Y_dif-y_val >= -player2_Y_default+10:
+        player2_Y_dif -= y_val
+
+def move_down2(y_val):
+    global player2_Y_dif
+    if player2_Y_dif+y_val <= player2_Y_default-10:
+        player2_Y_dif += y_val
+
 
 game_on = True
 
@@ -102,15 +106,14 @@ while game_on:
     elif key_input[pygame.K_s]:
         move_down(4)
     elif key_input[pygame.K_UP]:
-        move_up(4)
+        move_up2(4)
     elif key_input[pygame.K_DOWN]:
-        move_down(4)
+        move_down2(4)
     
     if key_input[pygame.K_5]:
         game_on=False
     player(player_Y_default+player_Y_dif)
+    player2(player2_Y_default+player2_Y_dif)
     calc_ball()
-    calc_cpu()
     ball()
-    cpu()
     pygame.display.update()
